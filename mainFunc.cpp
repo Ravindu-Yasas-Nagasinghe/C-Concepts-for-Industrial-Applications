38e44c611a76258a1f1e072dfbfc1cce2286567d
Ravindu Yasas Nagasinghe <56619402+Ravindu-Yasas-Nagasinghe@users.noreply.github.com>
2021-11-30 15:55:21 +0530

    Update mainFunc.cpp

--- mainFunc.cpp
+++ mainFunc.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 using namespace std;
+
 int main(){
     OldCoffeeMachine* OldMachine = new OldCoffeeMachine();
     CoffeeTouchscreenAdapter* adaptor = new CoffeeTouchscreenAdapter();
 
-    adaptor->connect(OldMachine)
+    adaptor->connect(OldMachine);
 
-    CoffeeMachineInterface* touchMachine = new CoffeeMachineInterface(adaptor);
+    CoffeeMachineInterface* touchMachine = new CoffeeMachineInterface();
 
       touchMachine->chooseFirstSelection();
       touchMachine->chooseSecondSelection();
       return 0;
     }
+
